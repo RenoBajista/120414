583fdd7a37693e59de1f175f590d239f13c73b7c
RenoBajista <reno.bajista@gmail.com>
2014-04-13 00:25:36 -0300

    Commit 1

--- src/HolusMundus.java
+++ src/HolusMundus.java
@@ -1,7 +1,7 @@
 public class HolusMundus {
 	public static void main(String args[]){
 		
-		System.out.println ("Esta es una prueba de commit...");
+		System.out.println ("... así que cambié esto");
 	}
 
 }
